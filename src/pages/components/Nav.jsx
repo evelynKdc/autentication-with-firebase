3ea71eb63494e2f3edf11331e9e62de26a15b386
Evelyn De la cruz <evelyndc_10@hotamil.com>
2023-04-21 00:07:41 -0500

    feat: Add nav component

---
+++ src/pages/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <nav>
+        <p>Autentication project</p>
+        <a href="#">Mi perfil</a>
+        <a href="#">Log out</a>
+    </nav>
+  )
+}
